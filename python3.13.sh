f403ef7966ff7a4fb917ffe8be97ecd2ba62ef33
xiuxi <xiuxi554325746@gmail.com>
2026-08-03 17:14:59 +0800

    a

---
+++ python3.13.sh
@@ -0,0 +1,12 @@
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.13 python3.13-venv python3.13-dev
+python3.13 --version
+python3.13 -m ensurepip --default-pip
+
+# Create the environment
+# python3.13 -m venv my_env
+
+# # Activate it
+# source my_env/bin/activate
+# Use code with caution.
--- youtube-dl.sh
+++ youtube-dl.sh
@@ -1,2 +1,15 @@
+# sudo add-apt-repository ppa:deadsnakes/ppa
+# sudo apt update
+# sudo apt install python3.13 python3.13-venv python3.13-dev
+# python3.13 --version
+# python3.13 -m ensurepip --default-pip
+
+# # Create the environment
+# python3.13 -m venv my_env
+
+# # Activate it
+# source my_env/bin/activate
+# Use code with caution.
+
 wget wget https://github.com/yt-dlp/yt-dlp/releases/download/2026.07.04/yt-dlp_linux -o /bin/youtube-dl
 chmod a+x /bin/youtube-dl
